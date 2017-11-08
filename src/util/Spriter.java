0d31815741e0e0cb837ec0d727a870088383bbc9
Ramil Shaymardanov <ramilmsh@r.local>
2017-11-08 14:50:03 -0500

    ..

---
+++ src/util/Spriter.java
@@ -0,0 +1,15 @@
+package util;
+
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+
+public class Spriter {
+    HashMap<String, Image> cache;
+
+    Spriter() {
+        cache = new HashMap<>();
+    }
+
+
+}
