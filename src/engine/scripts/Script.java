2990f164dc5fbedfdea3c4970b3ce19d8766ae69
Albert Xue <asx@duke.edu>
2017-11-08 23:43:56 -0500

    entity code

--- src/engine/scripts/Script.java
+++ src/engine/scripts/Script.java
@@ -1,7 +1,19 @@
 package engine.scripts;
 
-public class Script {
-    public Script() {
+import engine.entities.Entity;
 
+/**
+ *
+ * @author Albert
+ */
+public abstract class Script implements IScript {
+    private Entity myEntity;
+
+    public Script(Entity entity) {
+        myEntity = entity;
+    }
+
+    protected Entity getEntity() {
+        return myEntity;
     }
 }
