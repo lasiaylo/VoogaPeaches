2a9b39cb6fd08dd69827f2eecea9aa5d6c9b74cb
Unknown <lasia.lo@duke.edu>
2017-11-10 19:34:34 -0500

    Collision

--- src/engine/scripts/HitBox.java
+++ src/engine/scripts/HitBox.java
@@ -5,64 +5,80 @@ import java.util.LinkedList;
 import java.util.List;
 
 import javafx.scene.shape.Shape;
+import util.math.num.Vector;
 
-
-/**Represents a collision hitbox
+/**
+ * Represents a collision hitbox
  * 
  * @author lasia
  * @author Albert
  */
 public class HitBox {
 	private String tag;
-	private String visitorTag;
+	private List<String> visitorTags;
 	private List<Shape> myShapes;
-	
-	
-	/** Creates a new Hitbox
-	 * @param List of JavaFX Shapes
+	private Vector myPosition;
+
+	/**
+	 * Creates a new Hitbox
+	 * 
+	 * @param List
+	 *            of JavaFX Shapes
 	 */
-	public HitBox(List<Shape> shapes) {
+	public HitBox(List<Shape> shapes, Vector pos) {
 		myShapes = shapes;
+		myPosition = pos;
 	}
-	
-	
-	/** Creates a new HitBox
-	 * @param JavaFX Shape
+
+	/**
+	 * Creates a new HitBox
+	 * 
+	 * @param JavaFX
+	 *            Shape
 	 */
-	public HitBox(Shape shape ) {
-		this(new ArrayList<Shape>() {{
-			add(shape);
-		}});
+	public HitBox(Shape shape, Vector pos) {
+		this(new ArrayList<Shape>() {
+			{
+				add(shape);
+			}
+		}, pos);
 	}
-	
-	
+
 	/**
 	 * @param HitBox
 	 * @return Boolean on whether it intersects
 	 */
 	public boolean checkIntersect(HitBox other) {
 		List<Shape> otherBox = other.getShapes();
-		
-		for(Shape myShape : myShapes ) {
-			for(Shape otherShape : otherBox) {
+
+		for (Shape myShape : myShapes) {
+			for (Shape otherShape : otherBox) {
 				if (myShape.getBoundsInParent().intersects(otherShape.getBoundsInParent())) {
-					
+
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	
-	public void setVisitor(String otherTag) {
-		visitorTag = otherTag;
+
+	public void addVisitor(String otherTag) {
+		visitorTags.add(otherTag);
 	}
-	
-	public String getVisitor() {
-		return visitorTag;
+
+	public List<String> getVisitor() {
+		return visitorTags;
 	}
-	
+
 	public List<Shape> getShapes() {
 		return myShapes;
+	}
+	
+	public void setPosition(Vector pos) {
+		myPosition = pos;
+	}
+	
+	public Vector getPosition() {
+		return myPosition;
 	}
 }
