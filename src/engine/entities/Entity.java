e802bc9481c9c05d23c9d5fae4ab93b836f18a69
Estelle He <wh103@duke.edu>
2017-11-10 20:13:32 -0500

    add layer and background

--- src/engine/entities/Entity.java
+++ src/engine/entities/Entity.java
@@ -19,76 +19,86 @@ import java.util.List;
  *
  */
 public class Entity {
-    private Vector myPosition;
-    private Vector myVelocity;
-    private Vector myAcceleration;
-    private Circle myHitBox;
-    private ImageView myImageView;
-    private boolean isStatic;
-    private List<IScript> myScripts;
+	private Vector myPosition;
+	private Vector myVelocity;
+	private Vector myAcceleration;
+	private Circle myHitBox;
+	private ImageView myImageView;
+	private boolean isStatic;
+	private List<IScript> myScripts;
 
-    /**
-     *  Creates a new Entity
-     * @param pos       Vector position of new Entity
-     * @param scripts   Scripts attached to new Entity
-     */
-    public Entity(Vector pos, List<IScript> scripts, Image image) {
-        myPosition = pos;
-        myScripts = scripts;
+	/**
+	 * Creates a new Entity
+	 * 
+	 * @param pos
+	 *            Vector position of new Entity
+	 * @param scripts
+	 *            Scripts attached to new Entity
+	 */
+	public Entity(Vector pos, List<IScript> scripts, Image image) {
+		myPosition = pos;
+		myScripts = scripts;
 
-        myImageView = new ImageView(image);
-        myImageView.setX(FXProcessing.getXImageCoord(pos.at(0), myImageView));
-        myImageView.setY(FXProcessing.getYImageCoord(pos.at(1), myImageView));
-        double hitRadius = (myImageView.getBoundsInLocal().getWidth() > myImageView.getBoundsInLocal().getHeight())
-                ? myImageView.getBoundsInLocal().getWidth() / 2 : myImageView.getBoundsInLocal().getHeight() / 2;
-        myHitBox = new Circle(pos.at(0), pos.at(1), hitRadius);
-    }
+		myImageView = new ImageView(image);
+		myImageView.setX(FXProcessing.getXImageCoord(pos.at(0), myImageView));
+		myImageView.setY(FXProcessing.getYImageCoord(pos.at(1), myImageView));
+		double hitRadius = (myImageView.getBoundsInLocal().getWidth() > myImageView.getBoundsInLocal().getHeight())
+				? myImageView.getBoundsInLocal().getWidth() / 2
+				: myImageView.getBoundsInLocal().getHeight() / 2;
+		myHitBox = new Circle(pos.at(0), pos.at(1), hitRadius);
+	}
 
-    /**
-     * Create a new Entity
-     * @param x         X position of new Entity
-     * @param y         Y position of new Entity
-     * @param scripts   Scripts attached to new Entity
-     */
-    public Entity(double x, double y, List<IScript> scripts, Image image) {
-        this(new Vector(x, y), scripts, image);
-    }
+	/**
+	 * Create a new Entity
+	 * 
+	 * @param x
+	 *            X position of new Entity
+	 * @param y
+	 *            Y position of new Entity
+	 * @param scripts
+	 *            Scripts attached to new Entity
+	 */
+	public Entity(double x, double y, List<IScript> scripts, Image image) {
+		this(new Vector(x, y), scripts, image);
+	}
 
-    /**
-     * @return  Vector position of this entity
-     */
-    public Vector getPosition() {
-        return myPosition;
-    }
-    
-    /**
-     * @param New position for this entity
-     */
-    public void setPosition(Vector newPos) {
-    	myPosition = newPos;
-    }
+	/**
+	 * @return Vector position of this entity
+	 */
+	public Vector getPosition() {
+		return myPosition;
+	}
 
-    /**
-     * run all scripts attached to the Entity
-     */
-    public void update() {
-        for(IScript s : myScripts) {
-            s.execute(this);
-        }
-    }
+	/**
+	 * @param New
+	 *            position for this entity
+	 */
+	public void setPosition(Vector newPos) {
+		myPosition = newPos;
+	}
 
-    /**
-     * @return  List of entity's scripts
-     */
-    public List<IScript> getScripts(){
-    	return myScripts;
-    }
-    
-    /**
-     * @return Whether the entity is static or not. If an entity is static, it just needs to be updated once.
-     */
-    public boolean isStatic() {
-    	return isStatic;
-    }
-    
+	/**
+	 * run all scripts attached to the Entity
+	 */
+	public void update() {
+		for (IScript s : myScripts) {
+			s.execute(this);
+		}
+	}
+
+	/**
+	 * @return List of entity's scripts
+	 */
+	public List<IScript> getScripts() {
+		return myScripts;
+	}
+
+	/**
+	 * @return Whether the entity is static or not. If an entity is static, it just
+	 *         needs to be updated once.
+	 */
+	public boolean isStatic() {
+		return isStatic;
+	}
+
 }
