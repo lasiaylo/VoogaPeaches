f36bcdcc6629363a74d18f3bc942e44e6bd4ea48
Estelle He <wh103@duke.edu>
2017-11-11 22:51:02 -0500

    minor changes, more to come

--- src/authoring/PanelController.java
+++ src/authoring/PanelController.java
@@ -1,8 +1,17 @@
 package authoring;
 
+import engine.Engine;
+
 /**
  * TODO: Create the controller
  * @author Brian Nieves
+ * @author Estelle He
  */
 public class PanelController {
+	private Engine myEngine;
+	
+	public PanelController() {
+		myEngine = new Engine();
+	}
+	
 }
