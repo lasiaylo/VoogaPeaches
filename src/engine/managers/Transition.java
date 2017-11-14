6ca61a6645b577ede5ee4a7048ae25874b57a8c6
Unknown <lasia.lo@duke.edu>
2017-11-13 20:11:18 -0500

    Worked on transition - switching over to laptop

--- src/engine/managers/Transition.java
+++ src/engine/managers/Transition.java
@@ -1,5 +1,16 @@
 package engine.managers;
 
 public class Transition {
-
+	private State myDestinationState;
+	private Logic condition;
+	
+	public Transition(State state) {
+		myDestinationState = state;
+	}
+	
+	public boolean 
+	
+	public State getMyDestinationState() {
+		return myDestinationState;
+	}
 }
