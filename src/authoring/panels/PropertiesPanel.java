7af398d40d9c0e4783ce9efc24643f6a8eaa118e
Kelly Zhang <kz49@duke.edu>
2017-11-13 22:53:26 -0500

    setting up backbone for new PropertiesPanel

---
+++ src/authoring/panels/PropertiesPanel.java
@@ -0,0 +1,26 @@
+package authoring.panels;
+
+import authoring.Panel;
+import javafx.scene.layout.Region;
+
+public class PropertiesPanel implements Panel {
+
+	@Override
+	public Region getRegion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String title() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
