6cc2dd0da145ea7d9f5f3ba021b6d69cedf24fd3
Unknown <lasia.lo@duke.edu>
2017-11-17 11:13:13 -0500

    Outlined PropertiesPanel

--- src/authoring/panels/tabbable/PropertiesPanel.java
+++ src/authoring/panels/tabbable/PropertiesPanel.java
@@ -2,16 +2,23 @@ package authoring.panels.tabbable;
 
 import authoring.Panel;
 import authoring.ScreenPosition;
+import engine.entities.Entity;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 
+/**Displays the properties associated with a particular Entity
+ * @author lasia
+ *
+ */
 public class PropertiesPanel implements Panel {
+	private HBox myHBox;
+	private Entity myEntity;
 
 	@Override
 	public Region getRegion() {
-		// TODO Auto-generated method stub
-		return null;
+		return myHBox;
 	}
-
+	
 	@Override
 	public ScreenPosition getPosition() {
 		return ScreenPosition.TOP_RIGHT;
@@ -20,6 +27,49 @@ public class PropertiesPanel implements Panel {
 	@Override
 	public String title() {
 		return "Properties";
+	}
+	
+	/**Takes in an entity and displays its properties
+	 * @param entity
+	 */
+	public void updateProperties(Entity entity) {
+		myEntity = entity;
+		
+		addTransformProperty();
+		addRenderProperty();
+		addScriptProperties();
+		addButton();
+	}
+
+	/**Displays the Transform properties of an entity
+	 * 
+	 */
+	private void addTransformProperty() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**Displays the Render properties of an entity
+	 * 
+	 */
+	private void addRenderProperty() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**Displays the individual scripts of an entity
+	 * 
+	 */
+	private void addScriptProperties() {
+		// TODO Auto-generated method stub
+	}
+	
+	/**Displays a button that allows users to add more scripts to an entity
+	 * 
+	 */
+	private void addButton() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
