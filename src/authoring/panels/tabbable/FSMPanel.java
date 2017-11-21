3d85a411aee6cade0307539d6424ec6b33bd7bf2
Albert Xue <asx@duke.edu>
2017-11-20 20:03:06 -0500

    merge conflicts

--- src/authoring/panels/tabbable/FSMPanel.java
+++ src/authoring/panels/tabbable/FSMPanel.java
@@ -1,17 +1,11 @@
 package authoring.panels.tabbable;
 
 import authoring.Panel;
-import authoring.ScreenPosition;
 import javafx.scene.layout.Region;
 
 public class FSMPanel implements Panel {
     @Override
     public Region getRegion() {
-        return null;
-    }
-
-    @Override
-    public ScreenPosition getPosition() {
         return null;
     }
 
