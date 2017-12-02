72102ec1d97f90b40560bf5551809c522872feb4
Albert Xue <asx@duke.edu>
2017-12-02 14:06:57 -0500

    refactored/added hitbox functionality

--- tests/EventTest.java
+++ tests/EventTest.java
@@ -9,13 +9,22 @@ public class EventTest extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Entity entity = new Entity();
-        PrintEvent pEvent = new PrintEvent("no");
+        PrintEvent pEvent = new PrintEvent("print");
         entity.on("print", (event -> {
             PrintEvent printEvent = (PrintEvent) event;
             printEvent.printHellYeah();
         }));
 
+        Entity child = new Entity();
+        entity.add(child);
+
+        child.on("print", (event -> {
+            PrintEvent printEvent = (PrintEvent) event;
+            System.out.println("hell no");
+        }));
+
         entity.dispatchEvent(pEvent);
+        pEvent.fire(entity);
     }
 
     public static void main(String[] args) {
