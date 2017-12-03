d249920e1c2171233684676605cf720477899ce4
BillGatesNephew <wlkrwilletts@gmail.com>
2017-12-02 19:31:26 -0500

    Changed JSONToObjectConverter

--- src/database/jsonhelpers/JSONToObjectConverter.java
+++ src/database/jsonhelpers/JSONToObjectConverter.java
@@ -4,12 +4,9 @@ import database.examples.realtime.Post;
 import database.firebase.TrackableObject;
 import org.json.JSONObject;
 
-import javax.sound.midi.Track;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class JSONToObjectConverter<T extends TrackableObject> {
