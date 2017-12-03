cad3bc1b8bbf2d93302d711ed4e22e807afeb8d2
Richard Tseng <rt120@duke.edu>
2017-12-02 19:50:23 -0500

    FSM can publish current state, added StateEvent

---
+++ src/engine/events/StateEvent.java
@@ -0,0 +1,8 @@
+package engine.events;
+
+public class StateEvent extends Event{
+
+	public StateEvent(String type) {
+		super(type);
+	}
+}
---
+++ src/engine/fsm/State.java
@@ -0,0 +1,57 @@
+package engine.fsm;
+
+import com.google.gson.annotations.Expose;
+import database.firebase.TrackableObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**Represents a state within a Finite State Machine
+ * 
+ * @author lasia
+ *	@author Albert
+ * @author richardtseng
+ */
+public class State extends TrackableObject {
+	
+	@Expose private String stateName;
+	@Expose private List<Transition> myTransitions;
+
+	/**
+	 * Creates a new State
+	 */
+	public State() {
+		myTransitions = new ArrayList<>();
+	}
+	
+	/**
+	 * Creates a new State with a name
+	 */
+	public State(String state) {
+		stateName = state;
+		myTransitions = new ArrayList<>();
+	}
+	
+	/**Checks the transitions of this state to see if the conditions are met
+	 * If they are, the manager's current state will be changed to the transition's destination
+	 * @param manager
+	 */
+	public void update(FSM manager) {
+		for (Transition transition : myTransitions) {
+			if (transition.conditionsMeet(manager.getConditions())) {
+				manager.setCurrentState(transition.getDestinationState());
+			}
+		}
+	}
+	
+	/**
+	 * @return Transitions going out of this state
+	 */
+	public List<Transition> getTransitions(){
+		return myTransitions;
+	}
+	
+	public String getStateName() {
+		return stateName;
+	}
+}
