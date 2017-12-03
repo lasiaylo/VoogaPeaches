e628a9ea102a73525baf51ee304965d45354d05e
BillGatesNephew <wlkrwilletts@gmail.com>
2017-12-03 00:51:38 -0500

    Added comments to ScriptLoader class

--- src/database/scripthelpers/ScriptLoader.java
+++ src/database/scripthelpers/ScriptLoader.java
@@ -4,11 +4,22 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A class for caching Groovy scripts created by the user so
+ * that unnecessary I/O is not constantly being performed,
+ * which would waste resources
+ */
 public class ScriptLoader {
 
+    /* Final Variables */
     private static final Map<String, String> CACHED_SCRIPTS = loadAndCacheScripts();
     private static final String SCRIPT_PATH = "./data/scriptdata/";
 
+    /**
+     * Loads in the different Groovy files and saves them in a HashMap
+     * @return {@code Map<String, String>} of loaded groovy script strings
+     * corresponding to the name of a groovy script
+     */
     private static Map<String, String> loadAndCacheScripts() {
         Map<String, String> loadedFiles = new HashMap<>();
         File baseDirectory = new File(SCRIPT_PATH);
@@ -19,6 +30,13 @@ public class ScriptLoader {
         return loadedFiles;
     }
 
+    /**
+     * Reads in the strings for a groovy script line by line, and
+     * converts the script into a single string that is then stored
+     * @param groovyFile is a {@code File} representing the Groovy file
+     *                   to read from
+     * @return A {@code String} that contains all of the instructions
+     */
     private static String readStringForFile(File groovyFile) {
         String scriptString = "";
         String line = null;
@@ -35,6 +53,13 @@ public class ScriptLoader {
         }
     }
 
+    /**
+     * Retrieves the string with the groovy code that corresponds
+     * to the passed in file name
+     * @param filename is a {@code String} that represents the name
+     *                 of the Groovy Script that you want to retireve
+     * @return A {@code String} containing the groovy code to be executed
+     */
     public static String stringForFile(String filename) {
         return CACHED_SCRIPTS.get(filename);
     }
