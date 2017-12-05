1c5cb36e22fb3c162ee625462ed66f8419aae371
Albert Xue <asx@duke.edu>
2017-12-05 18:12:04 -0500

    hit box panle

--- src/authoring/panels/tabbable/HitBoxPanel.java
+++ src/authoring/panels/tabbable/HitBoxPanel.java
@@ -1,28 +1,56 @@
 package authoring.panels.tabbable;
 
 import authoring.Panel;
+import engine.collisions.HitBox;
 import engine.entities.Entity;
 import javafx.scene.Group;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HitBoxPanel implements Panel {
     private static final String TITLE = "Create or Add Hitboxes!";
     private Entity entity;
-    private Group group;
-    private List<Double> points;
+    private Group entityView = new Group();
+    private VBox region = new VBox();
+    private TextField textField = new TextField();
+    private List<Double> points = new ArrayList<>();
 
     public HitBoxPanel(Entity entity) {
         this.entity = entity;
-        this.group = new Group();
-        group.getChildren().add(entity.getNodes());
+        region.getChildren().add(entityView);
+        entityView.getChildren().add(entity.getNodes());
+        entityView.setOnMouseClicked(e -> addPoint(e));
+        region.getChildren().add(textField);
+    }
 
+    private void dragHandle(MouseDragEvent dragEvent) {
+        for(int i = 0; i < points.size(); i += 2) {
+            double x = points.get(i);
+            double y = points.get(i + 1);
+        }
+    }
+
+    private void addPoint(MouseEvent event) {
+        points.add(event.getX());
+        points.add(event.getY());
+    }
+
+    public void createHitBox() {
+        HitBox hitBox = new HitBox(points, (Double) entity.getProperty("x"),
+                (Double) entity.getProperty("y"), textField.getText());
+
+        entity.addHitBox(hitBox);
     }
 
     @Override
     public Region getRegion() {
-        return null;
+        return region;
     }
 
     @Override
