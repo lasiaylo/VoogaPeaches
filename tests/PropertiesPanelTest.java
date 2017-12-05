bc8f50c47cd42035061b015a0e4c6563675c04f6
Unknown <lasia.lo@duke.edu>
2017-12-05 18:10:32 -0500

    PropertiesPanelTest

---
+++ tests/PropertiesPanelTest.java
@@ -0,0 +1,45 @@
+import org.json.JSONObject;
+
+import authoring.panels.attributes.Attribute;
+import authoring.panels.tabbable.AttributesPanel;
+import database.jsonhelpers.JSONDataFolders;
+import database.jsonhelpers.JSONDataManager;
+import database.jsonhelpers.JSONToObjectConverter;
+import engine.entities.Entity;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PropertiesPanelTest extends Application{
+	AttributesPanel panel;
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		panel = new AttributesPanel();
+		panel.updateProperties(createEntity());
+		
+		setupStage();
+	
+	}
+
+	private void setupStage() {
+		Scene scene = new Scene(panel.getRegion());
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	private Entity createEntity() {
+		JSONDataManager manager = new JSONDataManager(JSONDataFolders.ENTITY_BLUEPRINT);
+        JSONObject blueprint = manager.readJSONFile("test");
+        JSONToObjectConverter<Entity> converter = new JSONToObjectConverter<>(Entity.class);
+
+        Entity entity = converter.createObjectFromJSON(Entity.class,blueprint);		
+		return entity;
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+	
+}
