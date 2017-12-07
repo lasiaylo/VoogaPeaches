55ec30058d88883e526001366aaea6eed16d7f73
Albert Xue <asx@duke.edu>
2017-12-07 17:36:01 -0500

    saving stuff to checkout branch

--- src/authoring/panels/tabbable/LibraryPanel.java
+++ src/authoring/panels/tabbable/LibraryPanel.java
@@ -1,8 +1,8 @@
 package authoring.panels.tabbable;
 
 
-import authoring.IPanelController;
 import authoring.Panel;
+import authoring.PanelController;
 import database.ObjectFactory;
 import database.filehelpers.FileDataFolders;
 import database.filehelpers.FileDataManager;
@@ -34,7 +34,7 @@ public class LibraryPanel implements Panel {
     private TilePane myTilePane;
     private ChoiceBox<String> myEntType;
     private VBox myArea;
-    private IPanelController myController;
+    private PanelController myController;
     private EntityManager myManager;
 
     public LibraryPanel() {
@@ -102,7 +102,7 @@ public class LibraryPanel implements Panel {
     }
 
     @Override
-    public void setController(IPanelController controller) {
+    public void setController(PanelController controller) {
         myController = controller;
         myManager = myController.getManager();
     }
