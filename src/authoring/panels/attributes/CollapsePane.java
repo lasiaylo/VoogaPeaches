e09a6b2ef4c01fb3f5b9b330a21ba22dc51ef10c
Unknown <lasia.lo@duke.edu>
2017-12-07 04:11:21 -0500

    Formatting

--- src/authoring/panels/attributes/CollapsePane.java
+++ src/authoring/panels/attributes/CollapsePane.java
@@ -6,30 +6,53 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import util.exceptions.GroovyInstantiationException;
 
 public class CollapsePane {
 	private final int LABEL_COL = 0;
 	private final int ATTRIBUTE_COL = 1;
-	private TitledPane myPane;
-	private GridPane myGrid;
+	private boolean collapse;
+	private TitledPane myCollapse;
+	private Pane myPane;
 	
-	public CollapsePane(Map<String,?> map, String title) throws GroovyInstantiationException {
-		myGrid = new GridPane();
-		myPane = new TitledPane(title, myGrid);
-		myPane.setAnimated(false);
+	public CollapsePane(Map<String,?> map, String title, boolean collapse) throws GroovyInstantiationException {
+		
+		myCollapse = new TitledPane(title, myPane);
+		myCollapse.setAnimated(false);
 		addMap(map);
 	}
 	
 	private void addMap(Map<String, ?> map) throws GroovyInstantiationException {
-		int row = 0;
-		for (String s : map.keySet()) {
-			myGrid.add(addLabel(s), LABEL_COL, row);
-			myGrid.add(addAttribute(map, s), ATTRIBUTE_COL, row);
-			row++;
-		}
+		if (collapse)
+			formatCollapse(map);
+		else
+			formatGrid(map);
 	}
 	
+	private void formatCollapse(Map<String, ?> map) throws GroovyInstantiationException {
+		VBox vBox = new VBox();
+		
+		for (String s : map.keySet()) {
+			Node node = addAttribute(map, s);
+			TitledPane title = new TitledPane(s, node);
+			vBox.getChildren().add(title);
+		}
+		myPane = vBox;
+	}
+
+	private void formatGrid(Map<String, ?> map) throws GroovyInstantiationException {
+		GridPane grid = new GridPane();
+		int row = 0;
+		for (String s : map.keySet()) {
+			grid.add(addLabel(s), LABEL_COL, row);
+			grid.add(addAttribute(map, s), ATTRIBUTE_COL, row);
+			row++;
+		}
+		myPane = grid;
+	}
+
 	private Label addLabel(String string) {
 		return new Label(string);
 	}
@@ -40,6 +63,6 @@ public class CollapsePane {
 	}
 
 	public Node getNode() {
-		return myPane;
+		return myCollapse;
 	}
 }
