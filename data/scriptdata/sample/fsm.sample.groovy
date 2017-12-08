40ef8e36172f618496ee56c9fdb9468492058c8e
Ramil Shaymardanov <ramilmsh@r.local>
2017-12-07 19:15:32 -0500

    working fsm

--- data/scriptdata/sample/fsm.sample.groovy
+++ data/scriptdata/sample/fsm.sample.groovy
@@ -9,7 +9,7 @@ fsm = new FSM(entity as Entity, [
                         default  : true
                 ],
                 transitions: [
-                        name2: "{entity -> return true }"
+                        name2: "{ entity -> println 1; return true }"
                 ]
         ],
         name2: [
@@ -17,7 +17,10 @@ fsm = new FSM(entity as Entity, [
                         property1: "bla2"
                 ],
                 transitions: [
-                        name1: "{ entity -> return false }"
+                        name1: "{ entity -> println 2; return true }"
                 ]
         ]
 ])
+
+for (int i = 0; i < 10; ++i)
+    fsm.step()
