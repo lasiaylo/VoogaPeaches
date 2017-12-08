8612ebaa6edfed9127741a46297b45cbbc2567f2
Ramil Shaymardanov <ramilmsh@r.local>
2017-12-07 19:12:14 -0500

    ..

--- data/scriptdata/sample/fsm.sample.groovy
+++ data/scriptdata/sample/fsm.sample.groovy
@@ -1,22 +1,23 @@
+import engine.entities.Entity
 import engine.fsm.FSM
+entity = new Entity()
 
-fsm = new FSM([
+fsm = new FSM(entity as Entity, [
         name1: [
                 properties : [
                         property1: "bla1",
-                        default: true
+                        default  : true
                 ],
                 transitions: [
-                        name2: "{ -> return true }"
+                        name2: "{entity -> return true }"
                 ]
         ],
         name2: [
                 properties : [
-                        property1: "bla2",
-                        default: false
+                        property1: "bla2"
                 ],
                 transitions: [
-                        name1: "{ -> return false }"
+                        name1: "{ entity -> return false }"
                 ]
         ]
 ])
