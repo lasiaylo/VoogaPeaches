67e66d74578ac2935f510d61edf058b4a16f332c
Unknown <lasia.lo@duke.edu>
2017-12-08 17:26:50 -0500

    ImageField + NullPointerFix

--- src/authoring/panels/attributes/ImageField.java
+++ src/authoring/panels/attributes/ImageField.java
@@ -2,6 +2,7 @@ package authoring.panels.attributes;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 public class ImageField extends Field {
     ImageView view;
@@ -25,10 +26,16 @@ public class ImageField extends Field {
     @Override
     protected void setControlAction() {
         view.setOnMouseClicked(e->chooseFile());
-
     }
 
     private void chooseFile() {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter JPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+        FileChooser.ExtensionFilter PNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+        FileChooser.ExtensionFilter GIF = new FileChooser.ExtensionFilter("GIF files (*.gif)", "*.GIF");
+        fileChooser.getExtensionFilters().addAll(JPG, PNG, GIF);
+        fileChooser.showOpenDialog(null);
+
     }
 
     @Override
