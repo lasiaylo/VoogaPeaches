9e1fc1302fba76c986d904bfc196aad652f28534
BillGatesNephew <wlkrwilletts@gmail.com>
2017-12-08 19:27:13 -0500

    Setup loading from firebase

--- src/database/GameLoader.java
+++ src/database/GameLoader.java
@@ -1,13 +1,19 @@
 package database;
 
+import database.filehelpers.FileConverter;
+import database.filehelpers.FileDataFolders;
+import database.filehelpers.FileDataManager;
+import database.firebase.FileStorageConnector;
 import database.jsonhelpers.JSONDataFolders;
 import database.jsonhelpers.JSONDataManager;
 import database.jsonhelpers.JSONToObjectConverter;
 import engine.entities.Entity;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import util.PropertiesReader;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * The class that handles the actual loading in of a game for the
@@ -28,12 +34,29 @@ public class GameLoader {
      */
     public GameLoader(String gameName) {
         JSONDataManager manager = new JSONDataManager(JSONDataFolders.GAMES);
+        JSONObject imageJSON = manager.readJSONFile(gameName + "/images.json");
+        loadGameImages(imageJSON);
         JSONObject gameJSON = manager.readJSONFile(gameName + "/root.json");
         gameRoot = (new JSONToObjectConverter<Entity>(Entity.class)).createObjectFromJSON(Entity.class, gameJSON);
     }
 
     /**
+     * Loads in the files from Firebase that are actually needed for a full game to be able to run
+     * @param images is a {@JSONObject} that represents the map obtained from the game's images.json file
+     */
+    private void loadGameImages(JSONObject images) {
+        FileStorageConnector connector = new FileStorageConnector();
+        FileDataManager manager = new FileDataManager(FileDataFolders.IMAGES);
+        List<Object> imageNames =  ((JSONArray) images.get("images")).toList();
+        for(Object image : imageNames){
+            String name = (String) image;
+            byte[] imageBytes = FileConverter.convertImageToByteArray(connector.retrieveImage(name));
+            manager.writeFileData(imageBytes, name);
+        }
+    }
+
+    /**
      * @return An {@code Entity} that represents the root of the game
      */
-    public Entity getRoot() { return gameRoot; }
+    public Entity loadGame() { return gameRoot; }
 }
