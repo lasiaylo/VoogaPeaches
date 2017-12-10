9a349abd0f77fa0b89eb7dea7d6220c7a00ef5ab
Simran Singh <esimran@gmail.com>
2017-12-10 15:03:26 -0500

    try to make FSM panel work

--- src/authoring/panels/FSM/FSMPanel.java
+++ src/authoring/panels/FSM/FSMPanel.java
@@ -1,9 +1,20 @@
 package authoring.panels.FSM;
 
+import authoring.fsm.FSMGraph;
 import authoring.panels.attributes.UpdatablePanel;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 
 public class FSMPanel implements UpdatablePanel {
+
+    private final String TITLE = "Properties";
+    private FSMGraph graph;
+
+    public FSMPanel() {
+        graph = new FSMGraph();
+    }
 
     @Override
     public void updateProperties() {
@@ -12,11 +23,20 @@ public class FSMPanel implements UpdatablePanel {
 
     @Override
     public Region getRegion() {
-        return null;
+        Button button = new Button("FSM");
+
+        Pane pane = new Pane(graph.getRender());
+
+        pane.setOnMouseClicked(graph::onSceneClick);
+        VBox box = new VBox(button, pane);
+
+
+
+        return button;
     }
 
     @Override
     public String title() {
-        return null;
+        return TITLE;
     }
 }
