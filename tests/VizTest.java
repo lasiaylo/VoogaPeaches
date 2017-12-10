f036c43166818ea266851efe2bb95c420b01b3d2
Albert Xue <asx@duke.edu>
2017-12-10 00:41:27 -0500

    fixed library panel

--- tests/VizTest.java
+++ tests/VizTest.java
@@ -1,5 +1,5 @@
 import engine.entities.Entity;
-//import engine.visualization.GameVisualizer;
+import engine.visualization.GameVisualizer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -7,25 +7,25 @@ import javafx.stage.Stage;
 public class VizTest extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        Entity root = new Entity();
-//
-//        Entity child1 = new Entity(root);
-//        Entity child2 = new Entity(root);
-//
-//        for (int i = 0; i < 5; i++){
-//            Entity e = new Entity(root);
-//        }
-//
-//        Entity child11 = new Entity(child1);
-//        Entity child12 = new Entity(child2);
-//
-//        GameVisualizer gameVisualizer = new GameVisualizer(root);
-//
-//        Scene s = new Scene(gameVisualizer.getGroup());
-//        primaryStage.setScene(s);
-//        primaryStage.setHeight(800);
-//        primaryStage.setWidth(800);
-//        primaryStage.show();
+        Entity root = new Entity();
+
+        Entity child1 = new Entity(root);
+        Entity child2 = new Entity(root);
+
+        for (int i = 0; i < 5; i++){
+            Entity e = new Entity(root);
+        }
+
+        Entity child11 = new Entity(child1);
+        Entity child12 = new Entity(child2);
+
+        GameVisualizer gameVisualizer = new GameVisualizer(root);
+
+        Scene s = new Scene(gameVisualizer.getGroup());
+        primaryStage.setScene(s);
+        primaryStage.setHeight(800);
+        primaryStage.setWidth(800);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
