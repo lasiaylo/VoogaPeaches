f2ac7adddcab05e1a7803eebc654eb52dd689930
Simran Singh <esimran@gmail.com>
2017-12-10 15:17:40 -0500

    up to date with engine

--- src/authoring/panels/attributes/Updatable.java
+++ src/authoring/panels/attributes/Updatable.java
@@ -1,10 +1,5 @@
 package authoring.panels.attributes;
 
 public interface Updatable {
-<<<<<<< HEAD
     void update();
 }
-=======
-    public abstract void update();
-}
->>>>>>> 455409d8fe0c02a994ac5c47117a9afd969f420f
