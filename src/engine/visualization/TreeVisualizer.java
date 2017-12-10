45582b4e0273ca4c4c37d8c7202c71de5ce7f43e
Richard Tseng <rt120@duke.edu>
2017-12-10 01:34:53 -0500

    Half working

--- src/engine/visualization/TreeVisualizer.java
+++ src/engine/visualization/TreeVisualizer.java
@@ -1,12 +1,14 @@
 package engine.visualization;
 
 import javafx.scene.Group;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import util.math.num.Vector;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import static engine.visualization.EntityViz.CONNECTION_LENGTH;
@@ -16,39 +18,36 @@ public class TreeVisualizer {
     private GameVisualizer gameVisualizer;
     private Visualizer visualizer;
     private Group group;
-    private Collection<Line> connections;
 
     public TreeVisualizer(GameVisualizer gameVisualizer, Visualizer visualizer){
         this.gameVisualizer = gameVisualizer;
         this.visualizer = visualizer;
         group = new Group();
-        connections = new ArrayList<>();
-        linesFromCircle(visualizer.getGroup(), visualizer.getNumChildren());
+        styleRoot(visualizer);
+        recurse(visualizer);
+    }
+
+    private void styleRoot(Visualizer visualizer){
+        //visualizer.getGroup().relocate(0, 0);
+        Circle circle = (Circle) visualizer.getGroup().getChildren().get(0);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
     }
 
     private void recurse(Visualizer visualizer){
-        //visualizer.getGroup().relocate();
+        group.getChildren().add(visualizer.getGroup());
         if (visualizer.getNumChildren() <= 0){
             return;
         }
-        group.getChildren().add(visualizer.getGroup());
-
-    }
-
-    private void linesFromCircle(Group group, int numLines){
-        Circle origin = (Circle) group.getChildren().get(0);
-        for (int i = 0; i < numLines; i++){
-            double angle = (2 * Math.PI)/(numLines)*i;
-            Vector radialOffset = vecFromHypotenuse(new Vector(origin.getCenterX(), origin.getCenterY()), origin.getRadius(), angle);
-            Vector lineEnd = vecFromHypotenuse(radialOffset, CONNECTION_LENGTH, angle);
-            Line line = new Line(radialOffset.at(0), radialOffset.at(1), lineEnd.at(0), lineEnd.at(1));
-            connections.add(line);
-            group.getChildren().add(line);
+        else{
+            for (int i = 0; i < visualizer.getChildrenList().size(); i++) {
+                Visualizer child = visualizer.getChildrenList().get(i);
+                Line l = visualizer.getLines().get(i);
+                child.getGroup().relocate(l.getEndX(), l.getEndY());
+                ((Circle) child.getGroup().getChildren().get(0)).setFill(Color.ORCHID);
+                recurse(child);
+            }
         }
-    }
-
-    private Vector vecFromHypotenuse(Vector oldPosition, double length, double angle) {
-        return oldPosition.add(new Vector(length * Math.cos(angle), length * Math.sin(angle)));
     }
 
     public Group getGroup() {
