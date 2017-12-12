a971bd2db3b5aec6d75efc2dc84c28b2a9eec75a
Simran Singh <esimran@gmail.com>
2017-12-11 19:21:22 -0500

    naming FSM

--- src/authoring/panels/tabbable/FSMPanel.java
+++ src/authoring/panels/tabbable/FSMPanel.java
@@ -2,9 +2,12 @@ package authoring.panels.tabbable;
 
 import authoring.fsm.FSMGraph;
 import authoring.panels.attributes.UpdatablePanel;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -12,10 +15,17 @@ import javafx.stage.Stage;
 public class FSMPanel implements UpdatablePanel {
 
     private final String TITLE = "FSM Panel";
-    private FSMGraph graph;
+    private VBox box;
+    private Stage stage;
 
     public FSMPanel() {
-        graph = new FSMGraph();
+        Button button = new Button("Add New FSM");
+        button.setMinHeight(50);
+        button.setMinWidth(50);
+        box = new VBox(button);
+        box.setFillWidth(true);
+        button.setOnMouseClicked(e -> prompt());
+        stage = new Stage();
     }
 
     @Override
@@ -23,22 +33,34 @@ public class FSMPanel implements UpdatablePanel {
 
     @Override
     public Region getRegion() {
-        Button button = new Button("Add New FSM");
-        button.setMinHeight(50);
-        button.setMinWidth(50);
-        VBox box = new VBox(button);
-        box.setFillWidth(true);
-        button.setOnMouseClicked(e -> createNewFSM());
         return box;
     }
 
-    private void createNewFSM() {
-        Stage stage = new Stage();
-        FSMGraph graph = new FSMGraph();
+    private void prompt() {
+        Scene scene = new Scene(new Group());
+        FlowPane flow = createPopup();
+        scene.setRoot(flow);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private FlowPane createPopup() {
+        FlowPane flow = new FlowPane();
+        flow.setMinSize(100, 200);
+        Button start = new Button("Create FSM");
+        TextField name = new TextField();
+        name.setPromptText("Enter your FSM name");
+        start.setOnMouseClicked(e -> createNewFSM(name.getText()));
+        flow.getChildren().addAll(start, name);
+        return flow;
+    }
+
+    private void createNewFSM(String name) {
+        FSMGraph graph = new FSMGraph(name);
         Scene s = new Scene(graph.getRender());
         s.addEventFilter(MouseEvent.MOUSE_PRESSED, graph::onSceneClick);
+        stage.setTitle(name);
         stage.setScene(s);
-        stage.show();
         stage.setOnHidden(e -> graph.export());
     }
 
