cdaa0b092f5da263d0081a4e7ce094c4ed9ff660
Richard Tseng <rt120@duke.edu>
2017-12-13 23:11:25 -0500

    viz

--- src/engine/visualization/GameVisualizer.java
+++ src/engine/visualization/GameVisualizer.java
@@ -1,6 +1,7 @@
 package engine.visualization;
 
 import engine.entities.Entity;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 
 public class GameVisualizer {
@@ -8,28 +9,29 @@ public class GameVisualizer {
     private Entity root;
     private EntityVisualizer rootVisualizer;
     private Group group;
-    //private TreeVisualizer treeVisualizer;
+    private TreeVisualizer treeVisualizer;
 
     public GameVisualizer(Entity root) {
         this.root = root;
         this.group = new Group();
         rootVisualizer = new EntityVisualizer(this, null, root);
         focus(rootVisualizer);
-//        Visualizer visualizer = new Visualizer(root, null);
-//        treeVisualizer = new TreeVisualizer(this, visualizer);
-//        focus(visualizer);
+
+        Visualizer visualizer = new Visualizer(root, null);
+        treeVisualizer = new TreeVisualizer(this, visualizer);
+        focus(treeVisualizer);
     }
 
     protected void focus(EntityVisualizer entityVisualizer) {
         group.getChildren().clear();
         group.getChildren().add(entityVisualizer.getGroup());
-        entityVisualizer.getGroup().relocate(200, 200);
+        entityVisualizer.getGroup().relocate( 225, 200);
     }
 
-    protected void focus(Visualizer visualizer) {
+    protected void focus(TreeVisualizer treeVisualizer) {
         group.getChildren().clear();
-        group.getChildren().add(visualizer.getGroup());
-        visualizer.getGroup().relocate(250, 250);
+        group.getChildren().add(treeVisualizer.getGroup());
+        treeVisualizer.getGroup().relocate(225, 200);
     }
 
     public Group getGroup() {
--- tests/VizTest.java
+++ tests/VizTest.java
@@ -8,55 +8,21 @@ public class VizTest extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Entity root = new Entity();
-
-        System.out.println("root:" + root.UIDforObject());
-
-
-        Entity child1 = new Entity(root);
-        Entity child2 = new Entity(root);
-
-        System.out.println("child 1:" + child1.UIDforObject());
-        System.out.println("child 2:" + child2.UIDforObject());
-
-
         for (int i = 0; i < 5; i++){
-            Entity e = new Entity(root);
-            System.out.println(i + ". " + e.UIDforObject());
+            Entity child = new Entity(root);
+            for(int j = 0; j < 3; j++){
+                Entity grandchild = new Entity(child);
+                for(int k = 0; k < 5; k++){
+                    Entity grandgrandchild = new Entity(grandchild);
+                }
+            }
         }
-
-        Entity child11 = new Entity(child1);
-        Entity child12 = new Entity(child2);
-
-        System.out.println("grandchild 1:" + child11.UIDforObject());
-        System.out.println("grandchild 2:" + child12.UIDforObject());
-
-
         GameVisualizer gameVisualizer = new GameVisualizer(root);
-
         Scene s = new Scene(gameVisualizer.getGroup());
         primaryStage.setScene(s);
         primaryStage.setHeight(800);
         primaryStage.setWidth(800);
         primaryStage.show();
-
-
-//        Entity root = new Entity();
-//        Entity child1 = new Entity(root);
-//        Entity child2 = new Entity(root);
-////        for (int i = 0; i < 4; i++){
-////            Entity e = new Entity(root);
-////            Entity ee = new Entity(e);
-////        }
-//        Entity grandChild1 = new Entity(child1);
-//        Entity grandChild2 = new Entity(child1);
-//        Entity grandChild3 = new Entity(child2);
-//        Entity grandChild4 = new Entity(child2);
-//        GameVisualizer gameVisualizer = new GameVisualizer(root);
-//        Scene s = new Scene(gameVisualizer.getGroup());
-//        primaryStage.setScene(s);
-//        primaryStage.setHeight(800);
-//        primaryStage.setWidth(800);
-//        primaryStage.show();
     }
 
     public static void main(String[] args) {
