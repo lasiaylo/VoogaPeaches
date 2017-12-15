c19028ae070bbfbe6a11c5f7cd05ebe1aec7505f
Kelly Zhang <kz49@duke.edu>
2017-12-15 12:36:31 -0500

    working on getting coins to randomly appear

--- data/filedata/scripts/kellyClick.groovy
+++ data/filedata/scripts/kellyClick.groovy
@@ -1,11 +1,7 @@
 package filedata.scripts
 
-import database.ObjectFactory
 import engine.entities.Entity
-import engine.events.ClickEvent
 import engine.events.Event
-import engine.events.MousePressedEvent
-import util.math.num.Vector
 
 { Entity entity, Map<String, Object> bindings, Event event ->
     entity.getParent().remove(entity)
