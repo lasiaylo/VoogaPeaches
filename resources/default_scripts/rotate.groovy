412f5d45822b5031065d25ab02693acd76f4aa99
Richard Tseng <rt120@duke.edu>
2017-12-14 20:18:17 -0500

    i have lupus

--- resources/default_scripts/rotate.groovy
+++ resources/default_scripts/rotate.groovy
@@ -1,4 +1,4 @@
-package default_scripts
+package scripts
 
 import engine.entities.Entity
 import engine.events.Event
@@ -6,15 +6,15 @@ import engine.events.EventType
 import engine.events.KeyPressEvent
 import javafx.scene.input.KeyCode
 
-    { Entity entity, Map<String, Object> bindings, Event event ->
-        entity = (Entity) entity;
-        entity.on(EventType.KEY_PRESS.getType(), {Event e ->
-            KeyPressEvent k = KeyPressEvent e
-            if (k.getKeyCode().equals(KeyCode.KP_RIGHT)){
-                entity.getNodes().setRotate(entity.getNodes().getRotate() + 90);
-            }
-            else if (k.getKeyCode().equals(KeyCode.KP_LEFT)){
-                entity.getNodes().setRotate(entity.getNodes().getRotate() - 90);
-            }
-        })
-    }
+{ Entity entity, Map<String, Object> bindings, Event event = null ->
+    entity = (Entity) entity;
+    entity.on(EventType.KEY_PRESS.getType(), {Event e ->
+        KeyPressEvent k = (KeyPressEvent) e;
+        if (k.getKeyCode().equals(KeyCode.KP_RIGHT)){
+            entity.getNodes().setRotate(entity.getNodes().getRotate() + 90.0);
+        }
+        else if (k.getKeyCode().equals(KeyCode.KP_LEFT)){
+            entity.getNodes().setRotate(entity.getNodes().getRotate() - 90.0);
+        }
+    })
+}
