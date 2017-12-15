ca147c85f96e8a985b6407fd9f22e361b937089f
BillGatesNephew <wlkrwilletts@gmail.com>
2017-12-15 00:45:31 -0500

    Made kitty launcher

--- data/filedata/scripts/checkers_script.groovy
+++ data/filedata/scripts/checkers_script.groovy
@@ -9,16 +9,12 @@ import engine.events.InitialImageEvent
 import util.math.num.Vector
 
 { Entity entity, Map<String, Object> bindings, Event event = null ->
-    println("Fired")
-    Map<String, Object> newParams = new HashMap<>();
-    newParams.put("x", entity.getProperty("x"))
-    newParams.put("y",entity.getProperty("y"))
-    newParams.put("vx", 0.3)
     ObjectFactory o = new ObjectFactory("Cat1.json",JSONDataFolders.ENTITY_BLUEPRINT)
     Entity i = o.newObject()
-    println(JSONHelper.JSONForObject(i).toString(4))
     def pos = new Vector((double)entity.getProperty("x"), (double)entity.getProperty("y"))
     def size = new Vector((double)entity.getProperty("width"), (double)entity.getProperty("height"))
-    entity.add(i)
+    entity.parent.add(i)
+    i.setProperty("vx", 0.2)
+    i.setProperty("x",entity.getProperty("x") + 50)
 
 }
